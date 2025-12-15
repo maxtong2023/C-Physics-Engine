8c4a09c42892432936e1fdc7b1f83faecc6c4aaf
maxtong2023 <maxtong2027@u.northwestern.edu>
2025-12-15 14:45:55 -0500

    Initial Setup with CMake and Clang

---
+++ build/CMakeFiles/physics_engine.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/physics_engine.dir/src/main.cpp.o"
+  "CMakeFiles/physics_engine.dir/src/main.cpp.o.d"
+  "physics_engine"
+  "physics_engine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/physics_engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
